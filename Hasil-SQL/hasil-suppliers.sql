497c5aee538963619e6604342fc3348abd616360
mufis-coder <muhamadfikrisunandar@gmail.com>
2022-01-11 09:08:13 +0700

    add hasil-suppliers.sql

---
+++ Hasil-SQL/hasil-suppliers.sql
@@ -0,0 +1,40 @@
+INSERT INTO suppliers VALUES (31, 'IAPYGB3S', 'Dustin Shannon', 'Marketing Manager', '8438 Thompson Falls
+Whitefurt, AR 21633', 'Kaneton', 'MI', 84789, 'Saint Lucia', '001-373-237-7000', '2116830311', '#JTKOGBXJGD0');
+INSERT INTO suppliers VALUES (32, 'FGKAGBCQE8Q', 'Randy Carlson', 'Marketing Manager', '7597 Nicholas Circle
+Vaughnville, NJ 98144', 'Matthewview', 'nan', 07946, 'Papua New Guinea', '001-313-193-2284x83500', '001-479-493-3994', '#ROEZGB1V');
+INSERT INTO suppliers VALUES (33, 'KOLKGBDN', 'Amber Fernandez', 'Marketing Manager', '8056 David Estates Apt. 924
+South Shannon, IA 14024', 'West Scottburgh', 'nan', 76495, 'Angola', '(937)405-8593x4279', '+1-725-173-2301x8954', '#VTINGBAG5ZZ');
+INSERT INTO suppliers VALUES (34, 'CUYJGBJBQRD', 'Sabrina Oneill', 'Sales Manager', 'PSC 3296, Box 7372
+APO AA 79892', 'Tylerfort', 'LA', 94860, 'British Virgin Islands', '9467830305', '927.300.5749', '#JHWYGBHTRPU');
+INSERT INTO suppliers VALUES (35, 'SGWIGBD2', 'Debra Johnson', 'Marketing Manager', '857 Jeremy Common
+East Robinport, OK 07387', 'Michaelland', 'nan', 19011, 'Georgia', '(310)405-4040', '(861)033-3574', '#AUOXGBNLWU4');
+INSERT INTO suppliers VALUES (36, 'PYYGGBOV', 'James Reyes', 'Order Administrator', '3880 Emily Ranch Suite 837
+Simmonsfurt, WI 18110', 'Michellebury', 'nan', 44342, 'Antigua and Barbuda', '+1-955-984-6985x03970', '926-094-7129x021', '#JPIWGBHYEZ9');
+INSERT INTO suppliers VALUES (37, 'MZXRGB7C', 'Scott Frazier', 'Sales Representative', '468 Mark Junction
+Maryborough, WY 69825', 'Williamston', 'nan', 62146, 'Botswana', '+1-318-568-9170x49787', '(098)120-8599x223', '#ZZFFGBWIU3D');
+INSERT INTO suppliers VALUES (38, 'GWLIGBLEBJN', 'Danielle Allison', 'Sales Manager', 'PSC 2988, Box 5873
+APO AE 54213', 'Jillside', 'nan', 75274, 'Reunion', '+1-738-106-1583', '8559629576', '#OPMEGBDXBG5');
+INSERT INTO suppliers VALUES (39, 'EKNMGB7J', 'Robert Daniel', 'Marketing Manager', '249 Kevin Radial Apt. 016
+North Yolandastad, NJ 05629', 'Terrifurt', 'nan', 84321, 'China', '686.209.1043', '001-535-397-3764', '#HXXPGB8RZVA');
+INSERT INTO suppliers VALUES (40, 'FLKHGBJ7', 'Charles Chung', 'Sales Representative', '54272 Mclaughlin Wall Apt. 963
+Greenborough, WI 77486', 'South Raymond', 'nan', 35999, 'Madagascar', '+1-637-023-5011x59570', '295-646-4234x82581', '#VSSOGBIX');
+INSERT INTO suppliers VALUES (41, 'DBTAGB4QZ4Q', 'Julia Williams', 'Accounting Manager', '58947 Yolanda Course Apt. 724
+North Mary, TN 26446', 'Orozcoshire', 'nan', 38559, 'Burkina Faso', '(310)316-5324x918', '961.833.0282x3586', '#LUXSGBRL');
+INSERT INTO suppliers VALUES (42, 'PIGWGBVL', 'Barbara Ellis', 'Sales Manager', '43104 Roberts Trail Apt. 955
+Port Stevenburgh, IN 84746', 'Cherylmouth', 'Victoria', 48653, 'Estonia', '261-092-2434x17862', '001-505-593-8518', '#HLIEGBV132W');
+INSERT INTO suppliers VALUES (43, 'ZZPIGBMSK49', 'Monica Schmidt', 'Sales Representative', '17470 Le Roads Suite 267
+Smithbury, OR 83649', 'Shelbystad', 'nan', 60384, 'Cuba', '906-707-3520', '+1-187-592-4719x7426', '#KJADGBSB');
+INSERT INTO suppliers VALUES (44, 'LUTHGBI1', 'Kayla Hudson', 'Marketing Manager', 'USNS Brooks
+FPO AP 59867', 'Thomasport', 'nan', 64119, 'Madagascar', '657-706-0799x66999', '717-645-6918', '#DBSOGB5YAM4');
+INSERT INTO suppliers VALUES (45, 'XUZEGBB9', 'Amanda Schwartz', 'Sales Agent', '84304 Suzanne Points Suite 008
+Reginatown, TX 54738', 'Nathanborough', 'nan', 35774, 'United States Minor Outlying Islands', '+1-869-489-7013x735', '701.326.5282x7691', '#VWWYGBKSZW3');
+INSERT INTO suppliers VALUES (46, 'AEYGGBK7P3B', 'Richard Jenkins', 'Sales Representative', '7734 Tyler Lodge Apt. 813
+New Troyfurt, MA 36055', 'Hudsonville', 'nan', 47894, 'Vietnam', '001-823-858-9861x2995', '+1-744-950-6588x398', '#JAVHGBFNVXG');
+INSERT INTO suppliers VALUES (47, 'SNZVGBA6', 'Jennifer Paul', 'Marketing Manager', '563 Weiss Islands
+North Amychester, NJ 69478', 'Kimberlyview', 'nan', 66149, 'Poland', '202-480-0878', '6050083897', '#IGQJGBGY');
+INSERT INTO suppliers VALUES (48, 'ECKAGB2Z4U7', 'Jessica Roberts DVM', 'Coordinator Foreign Markets', 'USCGC King
+FPO AE 34503', 'Alexanderborough', 'nan', 59796, 'Saudi Arabia', '(288)093-6019x750', '385.801.0211', '#TAPIGB8YLQT');
+INSERT INTO suppliers VALUES (49, 'VSWVGBWMHBW', 'Jose Moore', 'Marketing Manager', '303 Vazquez Views
+Clarkbury, VT 07910', 'Sarahville', 'Asturias', 56449, 'Brunei Darussalam', '683-177-8266x4751', '370-185-5960x43079', '#CHYKGBMWU6C');
+INSERT INTO suppliers VALUES (50, 'EKGEGBGU', 'Todd Ford', 'Marketing Manager', '381 Charles Trafficway
+Mullenbury, AZ 97522', 'East Carol', 'nan', 92591, 'Cape Verde', '118-204-5474x101', '3102614355', '#UTCYGBVNUDJ');
